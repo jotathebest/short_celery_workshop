5cc16083869efba7bb80211a8541855be83dc25b
Jose Garcia <reyes2000jose@gmail.com>
2023-08-01 11:59:44 -0500

    Adding example for problem-1

--- tasks.py
+++ tasks.py
@@ -1,9 +1,22 @@
+import datetime
+import logging
+import time
+
 from celery import Celery
 
-app = Celery('hello', broker='redis://localhost')
-app.conf.broker_url = 'redis://localhost:6379/0'
+app = Celery("hello", broker="redis://localhost")
+app.conf.broker_url = "redis://localhost:6379/0"
+
+logger = logging.getLogger(__name__)
 
 
 @app.task
-def hello():
-    return 'hello world'
+def timer(task_id: str, timeout: int = 10):
+    logger.info(f"Starting task with id {task_id}")
+    now = datetime.datetime.utcnow()
+    end = now + datetime.timedelta(seconds=timeout)
+    while now < end:
+        logger.info(f"waiting for {task_id} to finish, it should finish at {end.isoformat()}")
+        time.sleep(5)
+        now = datetime.datetime.utcnow()
+    logger.info(f"finished task with id {task_id}")
--- triggers.py
+++ triggers.py
@@ -1,3 +1,12 @@
-from tasks import hello
+import uuid
 
-hello.delay()
+from tasks import timer
+
+
+def trigger():
+    task_id = str(uuid.uuid4())
+    timeout = 10  # seconds
+    timer.delay(task_id=task_id, timeout=timeout)
+
+
+trigger()
